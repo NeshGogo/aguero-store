7be7ab2ea1dca659d8b9f0f1efc9731e6968ac9b
NeshGogo <rafaelaguero10@hotmail.com>
2020-12-27 14:43:07 -0400

    Primeros pasos con angular forms

--- src/app/admin/components/basic-forms/basic-forms.component.ts
+++ src/app/admin/components/basic-forms/basic-forms.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-basic-forms',
   templateUrl: './basic-forms.component.html',
-  styleUrls: ['./basic-forms.component.scss']
+  styleUrls: ['./basic-forms.component.scss'],
 })
 export class BasicFormsComponent implements OnInit {
-
-  constructor() { }
+  nameField: FormControl = new FormControl('',[Validators.required, Validators.maxLength(10)]);
+  numberField: FormControl = new FormControl();
+  telField: FormControl = new FormControl();
+  emailField: FormControl = new FormControl();
+  colorField: FormControl = new FormControl();
+  dateField: FormControl = new FormControl();
+  ageField: FormControl = new FormControl();
+  categoryField: FormControl = new FormControl('categoria-4');
+  tagField: FormControl = new FormControl('tag-4');
+  agreeField: FormControl = new FormControl(false);
+  genderField: FormControl = new FormControl('male');
+  zoneField: FormControl = new FormControl('');
+  constructor() {}
 
   ngOnInit(): void {
+    this.nameField.valueChanges.subscribe((value) => console.log(value));
   }
 
+  getNameValue() {
+    console.log(this.nameField.value);
+  }
+
+  get isNameFieldValid(){
+    return this.nameField.touched && this.nameField.valid;
+  }
+  get isNameFieldInValid(){
+    return this.nameField.touched && this.nameField.invalid;
+  }
 }
